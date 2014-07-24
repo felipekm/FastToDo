4d7260276e8d360cbbee3ebd673fdd1529faa2bc
felipekm <felipe.kautzmann@gmail.com>
2014-07-24 20:21:31 -0300

    Version adjust

---
+++ app/src/js/controllers/detailsController.js
@@ -0,0 +1,14 @@
+/*global angular, console, confirm*/
+
+angular.module("FastToDo").controller("detailsController", function ($scope) {
+    'use strict';
+
+    $scope.about = function about() {
+        $scope.goTo('/about');
+    };
+
+    $scope.back = function back() {
+        $scope.goTo('/home');
+    }
+
+});
